87a13560cb8729bd1adb2b84ce9e279d7be69c11
RahulBiseriya <rahulmukeshkumar87@gmail.com>
2026-05-27 18:50:00 +0000

    datatypes done

---
+++ basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) // we are using node js not browser 
+
+let name = "Rahul"
+let age = 18
+let isLoggedIn = true
+
+//numbers => 2 to the power of 53
+//bigInt => more than 2 to the power of 53
+//string => " "
+//boolean => true or false
+//null => standalone value
+//undefined => 
+//symbol => unique value
+
+//object
+
+console.log(typeof age)//number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
