e96a1896d73722c1f01f7a15c0d5c9f7c6c5f11c
RahulBiseriya <rahulmukeshkumar87@gmail.com>
2026-05-27 19:15:53 +0000

    conversion of data types

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = false
+// console.log(typeof score)
+// console.log(typeof (score))
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)// NaN => not a number
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 0 //fals
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+isLoggedIn = "" //false
+isLoggedIn ="abc" //true
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
