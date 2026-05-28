0528a2e066e3fc226ecd5b9876cf8ae53656eec9
Rahul <rahulmukeshkumar87@gmail.com>
2026-05-28 11:51:24 +0000

    conversion done

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -18,5 +18,37 @@ isLoggedIn ="abc" //true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+// console.log(2+3)
+// console.log(3 - 2)
+// console.log( 3*2)
+// console.log(3**2) // 3 to the power 2
+// console.log(3/2)
+// console.log(3%2)
+
+let s1 = "Rahul"
+let s2 = " Biseriya"
+
+// let newstring = s1 + s2
+// console.log(newstring)
+
+// console.log( 1 + "2")
+// console.log( "2" + 1)
+// console.log( 1 + 2 + "3")// 1 + 2 + "3" => 3 + "3" => 33    // priority first to number
+// console.log("1" + 2 + 3)// "1" + 2 + 3 => "1" + "2" + "3" => 123 // priority first to string
+// console.log(true)
+// console.log(+true)//1
+// console.log(+"abc")// NaN
+// console.log(+false)//0
+// console.log(+"")//0
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
